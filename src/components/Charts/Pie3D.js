fe8d03fe63dc999960f10de85dd3edd910abd2ae
KennyAzura <90944861+KennyAzura@users.noreply.github.com>
2022-08-13 10:17:37 +0700

    initial commit

---
+++ src/components/Charts/Pie3D.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const chartCommponent = ({data}) => {
+   const chartConfigs = {
+     type: "Pie3D", // The chart type
+     width: "100%", // Width of the chart
+     height: "400", // Height of the chart
+     dataFormat: "json", // Data type
+     dataSource: {
+       // Chart Configuration
+       chart: {
+         caption: "languages",
+         theme: "fusion",
+         decimals: 0,
+         pieRadius: "45%"
+       },
+       // Chart Data
+       data,
+     },
+   };
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default chartCommponent;
